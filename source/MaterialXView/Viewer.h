1070d79adb58cdc696f16d627873e60b32829a20
Bernard Kwok <Bernard.Kwok@autodesk.com>
2018-12-12 12:41:38 -0500

    Code cleanup.

--- source/MaterialXView/Viewer.h
+++ source/MaterialXView/Viewer.h
@@ -88,9 +88,7 @@ class Viewer : public ng::Screen
     bool setElementSelection(int index);
     void updateElementSelections();
     void updatePropertySheet();
-    void addValueToForm(mx::ValuePtr value, const std::string& label, const std::string& path, 
-                        mx::ValuePtr min, mx::ValuePtr max, const mx::StringVec& enumeration,
-                        const std::vector<mx::ValuePtr> enumValues, const std::string& group, ng::FormHelper& form);
+
   private:
     ng::Window* _window;
     std::unique_ptr<Mesh> _mesh;
